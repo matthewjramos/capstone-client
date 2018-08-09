7eadd6bcf757f969e3bb922ba7cdfdeb577e9a9e
matthewjramos <ramos.matthew.jorge@gmail.com>
2018-08-08 23:52:39 -0400

    Add basic authorization files, game logic

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,143 +1,117 @@
 'use strict'
 
-  // refer to the canvas element
-  var canvas = document.getElementById("myCanvas")
-  var ctx = canvas.getContext("2d")
+// refer to the canvas element
+const canvas = document.getElementById('myCanvas')
+const ctx = canvas.getContext('2d')
+let ballRadius = 10
+let x = canvas.width / 2
+let y = canvas.height - 30
+let dx = 2
+let dy = -2
+const wolf = new Image()
+wolf.src = './assets/wolf.png'
+const spriteHeight = 30
+const spriteWidth = 30
+let spriteX = (canvas.width - spriteWidth) / 2
+let spriteY = (canvas.height - spriteHeight) / 2
+console.log(canvas.height)
 
-  // define x and y
-  var x = canvas.width/2
-  var y = canvas.height-30
+let rightKey = false
+let leftKey = false
+let upKey = false
+let downKey = false
 
-  // differences in x and y (movement/speed)
-  var dx = -2
-  var dy = -2
+let frameCount = 0
 
-  //ball-radius for collision
-  var ballRadius = 10;
+document.addEventListener('keydown', keyDown, false)
+document.addEventListener('keyup', keyUp, false)
 
-  // for keyboard controls
-  let rightKey = false
-  let leftKey = false
-  let upKey = false
-  let downKey = false
-
-// listeners for keystrokes
-document.addEventListener("keydown", keyDownHandler, false)
-document.addEventListener("keyup", keyUpHandler, false)
-// listen for mouse
-// document.addEventListener("mousemove", mouseMoveHandler, false);
-
-function keyDownHandler (e) {
-  if (e.keyCode === 39) { rightKey = true }
-  else if (e.keyCode === 37) { leftKey = true }
-  if (e.keyCode === 38) {upKey = true}
-  else if (e.keyCode === 40) {downKey = true}
+function keyDown (e) {
+  if (e.keyCode === 39) rightKey = true
+  else if (e.keyCode === 37) leftKey = true
+  if (e.keyCode === 38) upKey = true
+  else if (e.keyCode === 40) downKey = true
 }
 
-function keyUpHandler(e) {
-  if (e.keyCode === 39) { rightKey = false }
-  else if (e.keyCode === 37) { leftKey = false }
-  if (e.keyCode === 38) {upKey = true}
-  else if (e.keyCode === 40) {downKey = true}
-  }
+function keyUp (e) {
+  if (e.keyCode === 39) rightKey = false
+  else if (e.keyCode === 37) leftKey = false
 
-  // collision detection
-  function collisionDetection() {
-      for(var c=0; c<brickColumnCount; c++) {
-          for(var r=0; r<brickRowCount; r++) {
-              var b = bricks[c][r];
-              if(b.status === 1) {
-               if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
-                   dy = -dy;
-                   b.status = 0;
-                   score++;
-                   if(score === brickRowCount*brickColumnCount) {
-                    alert("YOU WIN, CONGRATULATIONS!");
-                    document.location.reload();
-                  }
-               }
-             }
-          }
-      }
-  }
-
-  // keeping score
-  function drawScore() {
-    ctx.font = "16px Arial";
-    ctx.fillStyle = "#0095DD";
-    ctx.fillText("Score: "+score, 8, 20);
+  if (e.keyCode === 38) upKey = false
+  else if (e.keyCode === 40) downKey = false
 }
 
-$(function() {
-  window.keydown = {};
-
-  function keyName(event) {
-    return jQuery.hotkeys.specialKeys[event.which] ||
-      String.fromCharCode(event.which).toLowerCase();
-  }
-
-  $(document).bind("keydown", function(event) {
-    keydown[keyName(event)] = true;
-  });
-
-  $(document).bind("keyup", function(event) {
-    keydown[keyName(event)] = false;
-  });
-});
-
-// clear canvas, drawl ball, add movement
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  // drawBall();
-  // drawPaddle();
-  // drawScore();
-  // drawLives();
-  // collisionDetection();
-  // drawBricks();
-
-  // collision detection for ball
-  if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-    dx = -dx;
-  }
-  if(y + dy < ballRadius) {
-      dy = -dy;
-  // add lose condition, paddle + ball collision
-  } else if(y + dy > canvas.height-ballRadius) {
-    if(x > paddleX && x < paddleX + paddleWidth) {
-        dy = -dy;
-    }
-    else {
-      lives--;
-    if(!lives) {
-      alert("GAME OVER");
-      document.location.reload();
-    }
-    else {
-      x = canvas.width/2;
-      y = canvas.height-30;
-      dx = 2;
-      dy = -2;
-      paddleX = (canvas.width-paddleWidth)/2;
-    }
-    }
-  }
-
-  // sideways collision detection for the paddle
-  if(rightKey && paddleX < canvas.width-paddleWidth) {
-    paddleX += 7;
-  }
-  else if(leftKey && paddleX > 0) {
-    paddleX -= 7;
-  }
-
-  x += dx;
-  y += dy;
-
-  requestAnimationFrame(draw);
+function drawBall () {
+  ctx.beginPath()
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2)
+  ctx.fillStyle = '#0095DD'
+  ctx.fill()
+  ctx.closePath()
 }
 
+function drawSprite () {
+  ctx.beginPath()
+  ctx.rect(spriteX, spriteY, spriteWidth, spriteHeight)
+  ctx.fillStyle = '#0095DD'
+  ctx.fill()
+  ctx.closePath()
+}
+function drawScore () {
+  ctx.font = '16px Arial'
+  ctx.fillStyle = '#0095DD'
+  ctx.fillText('Score: ' + frameCount / 100, 8, 30)
+}
 
-  draw();
+function draw () {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  drawBall()
+  drawSprite()
+  drawScore()
+
+  if (x + dx > canvas.width || x + dx < 0) {
+    dx = -dx
+  }
+  if (y + dy > canvas.height || y + dy < 0) {
+    dy = -dy
+  }
+
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx
+  } if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    dy = -dy
+  } else if ((x > spriteX && x < spriteX + spriteWidth) && (y > spriteY && y < spriteY + spriteHeight)) {
+      $('#content').html('GAME OVER')
+      canvas.width = 0
+      // document.location.reload()
+  }
+
+  if (rightKey && spriteX < canvas.width - spriteWidth) {
+    spriteX += 5
+  } else if (leftKey && spriteX > 0) {
+    spriteX -= 5
+  } if (downKey && spriteY < canvas.height - spriteHeight) {
+    spriteY += 5
+  } else if (upKey && spriteY > 0) {
+    spriteY -= 5
+  }
+
+  x += dx
+  y += dy
+  requestAnimationFrame(draw)
+  frameCount++
+  // console.log(frameCount)
+  ballRadius += .01
+  dx += .5
+  // console.log(dx)
+  dy -= .1
+  // canvas.width -= .0000000000001
+  // console.log(dy)
+  // dy += .05
+  // spriteX += .05
+  // spriteY += .05
+}
+
+draw()
 
 module.exports = {
   draw: draw
